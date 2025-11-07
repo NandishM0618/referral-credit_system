dff1de948e4c1dc4ed673bae944a123f498084b4
NandishM0618 <nandish1729@gmail.com>
2025-11-07 20:08:21 +0530

    fix: url added for notifying mechanisim

--- src/frontend/app/lib/socket.ts
+++ src/frontend/app/lib/socket.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:8080", {
+const socket = io("https://referral-creditsystem-production.up.railway.app", {
     transports: ["websocket"]
 })
 
